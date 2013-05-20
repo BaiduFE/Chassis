b951f4153d7a8805958c2c1e8ffeca68b8212a87
Miller <chen.minliang@gmail.com>
2013-05-20 11:07:48 +0800

    created

---
+++ src/router/history.pushstate.js
@@ -0,0 +1,13 @@
+/**
+ * @fileOverview 使用pushstate实现的history
+ * @requires Router.History
+ */
+
+History.Pushstate = History.extend( ( function() {
+
+	var rTrailingSlash = /\/$/;
+
+	return {
+
+	};
+} )() );

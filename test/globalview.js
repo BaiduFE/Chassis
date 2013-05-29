e13ad920be307bd1f2c7afb6fdf596d048a45239
Miller <chen.minliang@gmail.com>
2013-05-29 17:30:56 +0800

    update testcases

--- test/globalview.js
+++ test/globalview.js
@@ -5,18 +5,18 @@ $(document).ready(function() {
   test( 'constructor', 3, function() {
 
     var Router = Chassis.Router.extend( {
-      routes: [ 'pagea/:aid', 'pageb/:bid' ]
+      routes: [ 'glvpagea1/:aid', 'glvpageb1/:bid' ]
     } );
 
     var rt = new Router();
 
     var gView = new Chassis.GlobalView( {
-      id: 'pagea',
-      className: 'pagea'
+      id: 'glvpagea1',
+      className: 'glvpagea1'
     }, rt );
 
-    equal( gView.$el[ 0 ].id, 'pagea' );
-    equal( gView.$el[ 0 ].className, 'pagea' );
+    equal( gView.$el[ 0 ].id, 'glvpagea1' );
+    equal( gView.$el[ 0 ].className, 'glvpagea1' );
     equal( gView.router, rt );
 
     Chassis.history.destroy();
@@ -27,23 +27,23 @@ $(document).ready(function() {
   asyncTest( 'routechange event and getCurrentAction', 5, function() {
 
     var Router = Chassis.Router.extend( {
-      routes: [ 'pagea/:aid', 'pageb/:bid' ]
+      routes: [ 'glvpagea2/:aid', 'glvpageb2/:bid' ]
     } );
 
-    Chassis.PageView[ 'pagea' ] = Chassis.PageView.extend( {} );
-    Chassis.PageView[ 'pageb' ] = Chassis.PageView.extend( {} );
+    Chassis.PageView[ 'glvpagea2' ] = Chassis.PageView.extend( {} );
+    Chassis.PageView[ 'glvpageb2' ] = Chassis.PageView.extend( {} );
 
     var gView = new Chassis.GlobalView( {
-      id: 'pagea',
-      className: 'pagea'
+      id: 'glvpagea2',
+      className: 'glvpagea2'
     }, new Router() );
 
     gView.on( 'routechange', function( opts ) {
       equal( opts.from, null );
-      equal( opts.to, opts.views[ 'pagea' ] );
+      equal( opts.to, opts.views[ 'glvpagea2' ] );
       equal( opts.params.aid, 1 );
       equal( opts.views, gView.router.views );
-      equal( gView.getCurrentAction(), 'pagea' );
+      equal( gView.getCurrentAction(), 'glvpagea2' );
 
       start();
       Chassis.history.navigate( '', { trigger: false } );
@@ -52,17 +52,17 @@ $(document).ready(function() {
 
     Chassis.history.start();
 
-    Chassis.history.navigate( 'pagea/1' );
+    Chassis.history.navigate( 'glvpagea2/1' );
 
   } );
   
   asyncTest( 'triggerPageEvent', 4, function() {
 
     var Router = Chassis.Router.extend( {
-      routes: [ 'pagea/:aid', 'pageb/:bid' ]
+      routes: [ 'glvpagea3/:aid', 'glvpageb3/:bid' ]
     } );
 
-    Chassis.PageView[ 'pagea' ] = Chassis.PageView.extend( {
+    Chassis.PageView[ 'glvpagea3' ] = Chassis.PageView.extend( {
       events: {
         'notice1 view': function( opts ) {
           equal( opts.signal, 1 );
@@ -72,7 +72,7 @@ $(document).ready(function() {
         }
       }
     } );
-    Chassis.PageView[ 'pageb' ] = Chassis.PageView.extend( {
+    Chassis.PageView[ 'glvpageb3' ] = Chassis.PageView.extend( {
       events: {
         'notice2 view': function( opts ) {
           equal( opts.signal, 2 );
@@ -84,17 +84,17 @@ $(document).ready(function() {
     } );
 
     var gView = new Chassis.GlobalView( {
-      id: 'pagea',
-      className: 'pagea'
+      id: 'glvpagea3',
+      className: 'glvpagea3'
     }, new Router() );
 
     gView.on( 'routechange', function( opts ) {
       
       if ( opts.from ) {
 
-        gView.triggerPageEvent( 'pagea', 'notice1', { signal: 1 } );
-        gView.triggerPageEvent( 'pageb', 'notice2', { signal: 2 } );
-        gView.triggerPageEvent( 'pagea,pageb', 'noticeall', { signal: 3 } );
+        gView.triggerPageEvent( 'glvpagea3', 'notice1', { signal: 1 } );
+        gView.triggerPageEvent( 'glvpageb3', 'notice2', { signal: 2 } );
+        gView.triggerPageEvent( 'glvpagea3,glvpageb3', 'noticeall', { signal: 3 } );
 
         start();
         Chassis.history.navigate( '', { trigger: false } );
@@ -105,8 +105,8 @@ $(document).ready(function() {
 
     Chassis.history.start();
 
-    Chassis.history.navigate( 'pagea/1' );
-    Chassis.history.navigate( 'pageb/1' );
+    Chassis.history.navigate( 'glvpagea3/1' );
+    Chassis.history.navigate( 'glvpageb3/1' );
 
   } );
 
